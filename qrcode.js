e121f17ad78d5a628b6ef8106c1afd05a213ac15
John-Clawd Comeau <jcclaw@clawd.unternet.net>
2026-03-08 15:58:14 +0100

    possible fix for BOM and utf-8 encoding by Claude

--- qrcode.js
+++ qrcode.js
@@ -31,36 +31,43 @@ var QRCode;
 		this.data = data;
 		this.parsedData = [];
 
-		// Added to support UTF-8 Characters
-		for (var i = 0, l = this.data.length; i < l; i++) {
-			var byteArray = [];
-			var code = this.data.charCodeAt(i);
+		if (QRCode.binaryMode) {
+			// binary mode: pass raw byte values, no UTF-8 encoding or BOM
+			for (var i = 0, l = this.data.length; i < l; i++) {
+				this.parsedData.push(this.data.charCodeAt(i) & 0xFF);
+			}
+		} else {
+			// Added to support UTF-8 Characters
+			for (var i = 0, l = this.data.length; i < l; i++) {
+				var byteArray = [];
+				var code = this.data.charCodeAt(i);
 
-			if (code > 0x10000) {
-				byteArray[0] = 0xF0 | ((code & 0x1C0000) >>> 18);
-				byteArray[1] = 0x80 | ((code & 0x3F000) >>> 12);
-				byteArray[2] = 0x80 | ((code & 0xFC0) >>> 6);
-				byteArray[3] = 0x80 | (code & 0x3F);
-			} else if (code > 0x800) {
-				byteArray[0] = 0xE0 | ((code & 0xF000) >>> 12);
-				byteArray[1] = 0x80 | ((code & 0xFC0) >>> 6);
-				byteArray[2] = 0x80 | (code & 0x3F);
-			} else if (code > 0x80) {
-				byteArray[0] = 0xC0 | ((code & 0x7C0) >>> 6);
-				byteArray[1] = 0x80 | (code & 0x3F);
-			} else {
-				byteArray[0] = code;
+				if (code > 0x10000) {
+					byteArray[0] = 0xF0 | ((code & 0x1C0000) >>> 18);
+					byteArray[1] = 0x80 | ((code & 0x3F000) >>> 12);
+					byteArray[2] = 0x80 | ((code & 0xFC0) >>> 6);
+					byteArray[3] = 0x80 | (code & 0x3F);
+				} else if (code > 0x800) {
+					byteArray[0] = 0xE0 | ((code & 0xF000) >>> 12);
+					byteArray[1] = 0x80 | ((code & 0xFC0) >>> 6);
+					byteArray[2] = 0x80 | (code & 0x3F);
+				} else if (code > 0x80) {
+					byteArray[0] = 0xC0 | ((code & 0x7C0) >>> 6);
+					byteArray[1] = 0x80 | (code & 0x3F);
+				} else {
+					byteArray[0] = code;
+				}
+
+				this.parsedData.push(byteArray);
 			}
 
-			this.parsedData.push(byteArray);
-		}
+			this.parsedData = Array.prototype.concat.apply([], this.parsedData);
 
-		this.parsedData = Array.prototype.concat.apply([], this.parsedData);
-
-		if (this.parsedData.length != this.data.length) {
-			this.parsedData.unshift(191);
-			this.parsedData.unshift(187);
-			this.parsedData.unshift(239);
+			if (this.parsedData.length != this.data.length) {
+				this.parsedData.unshift(191);
+				this.parsedData.unshift(187);
+				this.parsedData.unshift(239);
+			}
 		}
 	}
 
@@ -466,9 +473,9 @@ var QRCode;
 	 * @param {Number} nCorrectLevel
 	 * @return {Number} type
 	 */
-	function _getTypeNumber(sText, nCorrectLevel) {			
+	function _getTypeNumber(sText, nCorrectLevel) {
 		var nType = 1;
-		var length = _getUTF8Length(sText);
+		var length = QRCode.binaryMode ? sText.length : _getUTF8Length(sText);
 		
 		for (var i = 0, len = QRCodeLimitLength.length; i <= len; i++) {
 			var nLimit = 0;
